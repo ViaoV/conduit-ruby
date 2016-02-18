089be1c3e90cb5835be15b8d634a1fa2caa657c1
Joe Bellus <joe@5sigma.io>
2016-02-18 12:53:09 -0600

    Added basic API functions
    Conduit::Client has methods for most basic API functions

--- lib/conduit/conduit.rb
+++ lib/conduit/conduit.rb
@@ -8,6 +8,8 @@ require 'uri'
 
 module Conduit
 
+  class ApiError < StandardError; end
+
   class Client
     attr_accessor :host, :access_key_name, :access_key_secret
     def initialize(host, key_name, key_secret)
@@ -20,10 +22,8 @@ module Conduit
       request[:requestTime] = Time.new.strftime("%Y-%m-%dT%H:%M:%S%:z")
       request[:token] = SecureRandom.uuid
       data = request[:token] + request[:requestTime]
-      puts data
       hmac =  OpenSSL::HMAC.digest("sha256", @access_key_secret, data)
       request[:keyName] = @access_key_name
-      puts hmac.each_byte.map { |b| b.to_s(16) }.join
       request[:signature] = Base64.encode64(hmac)
     end
 
@@ -32,19 +32,58 @@ module Conduit
       req = Net::HTTP::Post.new(uri)
       req.content_type = 'application/json'
       sign! request
-      puts JSON.generate(request)
       req.body = JSON.generate(request)
       res = Net::HTTP.new(uri.hostname, uri.port).start {|h| h.request(req) }
-      JSON.parse(res.body)
+      if res.code == "200"
+        return JSON.parse(res.body)
+      end
+      if res.code == "404"
+        raise ApiError, "Endpoint not found"
+      end
+      if res.code == "400"
+        raise ApiError, JSON.parse(res.body)[:error]
+      end
+      puts res.code
     end
 
     def register(mailbox_name)
-      request "get", {mailbox: mailbox_name}
+      request "register", {mailbox: mailbox_name}
+    end
+
+    def deregister(mailbox_name)
+      request "deregister", {mailbox: mailbox_name}
     end
 
     def get_message()
-      request "register", {mailbox: mailbox_name}
+      request "get", {mailbox: mailbox_name}
     end
+
+    def system_stats()
+      request "stats", {}
+    end
+
+    def client_stats()
+      request "stats/clients", {}
+    end
+
+    def list_deployments(opts = {})
+      request = {}
+      request[:deploymentId] = opts[:id] || ""
+      request[:nameSearch] = opts[:name] || ""
+      request[:keySearch] = opts[:keyName] || ""
+      request[:count] = opts[:count] || 10
+      request "deploy/list", request
+    end
+
+    def deploy(mailboxes, script, opts = {})
+      request = {body: script}
+      request[:mailboxes] = mailboxes if mailboxes.is_a Array
+      request[:pattern] = mailboxes if mailboxes.is_a String
+      request[:deploymentName] = opts[:name] if opts[:name]
+      request[:asset] = opts[:asset] if opts[:asset]
+      request "put", request
+    end
+
   end
 
 end
